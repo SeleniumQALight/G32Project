18b1700b0517c72b29c956bc1c7eff1e6bc1dd4a
Taras_Radulenko <Taras_Radulenko@epam.com>
2018-03-20 18:15:18 +0200

    projs were added

---
+++ velichkoProj/src/main/java/velichkoProj/App.java
@@ -0,0 +1,13 @@
+package velichkoProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zhilaProj/src/main/java/zhilaProj/App.java
@@ -0,0 +1,13 @@
+package zhilaProj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
