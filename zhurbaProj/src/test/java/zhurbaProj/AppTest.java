5f4e1dce96bc101e64cd209cf102ed1182d43a52
Oleksandr Zhurba <runok.al@gmail.com>
2018-03-22 20:57:02 +0200

    Second Commit

--- zhurbaProj/src/test/java/zhurbaProj/AppTest.java
+++ zhurbaProj/src/test/java/zhurbaProj/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App. Test2203.
+ * Unit test for simple App. Test2203. One more changes.
  */
 public class AppTest 
     extends TestCase
