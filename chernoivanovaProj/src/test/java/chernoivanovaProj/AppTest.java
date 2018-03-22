20644a2fd18dc268d5cfcce5bcc2286c2feedbcd
Aleksandra Chornoivanova <achornoivanova@gmail.com>
2018-03-22 20:57:15 +0200

    first commit 2

--- chernoivanovaProj/src/test/java/chernoivanovaProj/AppTest.java
+++ chernoivanovaProj/src/test/java/chernoivanovaProj/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App. test 22-03
+ * Unit test for simple App. test 22-03-18
  */
 public class AppTest 
     extends TestCase
