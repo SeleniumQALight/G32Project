3d5dbb910ecdb002d418b658d5313cbd79461311
sameliuk <samov4448@gmail.com>
2018-03-23 21:29:00 +0200

    first commit 2

--- sameliukProj/src/test/java/sameliukProj/AppTest.java
+++ sameliukProj/src/test/java/sameliukProj/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App. Test comment.
+ * Unit test for simple App. Test text .
  */
 public class AppTest 
     extends TestCase
