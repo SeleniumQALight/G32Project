e13e07b89d61488bf0d18082705d332b9f066641
sameliuk <samov4448@gmail.com>
2018-04-10 21:46:05 +0300

    commit loginTest

---
+++ sameliukProj/src/test/java/loginTests/Login.java
@@ -0,0 +1,57 @@
+package loginTests;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+    WebDriver webDriver;
+
+    @Before
+    public void setUp() {
+        File file = new File("./src/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        webDriver = new ChromeDriver();
+    }
+
+    @Test
+    public void validLogin() {
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        webDriver.get("http://v3.test.itpmgroup.com");
+        webDriver.findElement(By.name("_username")).sendKeys("Student");
+        webDriver.findElement(By.id("password")).sendKeys("90909");
+        webDriver.findElement(By.tagName("Button")).click();
+
+        Assert.assertTrue("Avatat is notpresent",
+                isAvatarPresent());
+
+    }
+
+    @After
+    public void testDown() {
+        webDriver.quit();
+    }
+
+    private boolean isAvatarPresent() {
+        try {
+            return webDriver.findElement(By.xpath(".//div[@class='pull-left image']//img[@class='img-circle']")).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
+
