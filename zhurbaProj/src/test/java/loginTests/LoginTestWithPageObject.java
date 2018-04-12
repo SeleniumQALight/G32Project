929f0eb90b6c748ebfbdd22584af206c5d5d07bc
Oleksandr Zhurba <runok.al@gmail.com>
2018-04-12 19:53:11 +0300

    Automation LogIn

---
+++ zhurbaProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -0,0 +1,11 @@
+package loginTests;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class LoginTestWithPageObject extends ParentTest{
+    @Test
+    public void validLogIn (){
+
+    }
+}
