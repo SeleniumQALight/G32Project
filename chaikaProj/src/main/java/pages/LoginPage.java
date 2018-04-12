f8cb7ae04f09799064ca62200f3e32c95178a076
Oleksandr Chaika <chaykaa@ukr.net>
2018-04-12 21:36:49 +0300

    Commit 12.04.18

---
+++ chaikaProj/src/main/java/pages/LoginPage.java
@@ -0,0 +1,28 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage extends ParentPage{
+
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public void openPage() {
+        try {
+            webDriver.get("http://v3.test.itpmgroup.com");
+            logger.info("Login Page was opened");
+        } catch (Exception e) {
+            logger.error("Can not open LoginPage");
+            Assert.fail("Can not open LoginPage");
+        }
+    }
+    public void enterLogin(String login){
+        WebElement inputLogin = webDriver.findElement(By.name("_username"));
+        actionWithOurElements.enterTextInToElement(inputLogin, login);
+    }
+}
