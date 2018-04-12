03f821db90bffd33b829e1ef6912bd186e497851
Aleksandra Chornoivanova <achornoivanova@gmail.com>
2018-04-12 21:36:00 +0300

    Page Object - 12-04

---
+++ chernoivanovaProj/src/test/java/loginTests/LoginTestWithPageObject.java
@@ -0,0 +1,12 @@
+package loginTests;
+
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class LoginTestWithPageObject extends ParentTest {
+    @Test
+    public void validLogIn (){
+        loginPage.openPage();
+        loginPage.enterLogin("Student");
+    }
+}
