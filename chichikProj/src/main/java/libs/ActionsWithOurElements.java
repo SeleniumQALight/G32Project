f85e988be6f30800642ed391b8571e61598628c3
Andrey <andrey.chichick@railsreactor.com>
2018-04-16 17:16:26 +0300

    Enter password and click Submit button added

--- chichikProj/src/main/java/libs/ActionsWithOurElements.java
+++ chichikProj/src/main/java/libs/ActionsWithOurElements.java
@@ -25,4 +25,15 @@ public class ActionsWithOurElements {
             Assert.fail("Cannot work with element");
         }
     }
+
+
+    public void clickByElement(WebElement webElement){
+        try {
+            webElement.click();
+        }catch (Exception e){
+            logger.error("Cannot work with element");
+            Assert.fail("Cannot work with element");
+
+        }
+    }
 }
