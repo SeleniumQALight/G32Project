4531f2da2c428eca12b31dec87b51090910d7789
Levitskiy <mlevitskiy89@gmail.com>
2018-04-17 20:10:42 +0300

    @FindBy & printErrorAndStop

--- levitskiiProj/src/main/java/libs/ActionsWithOurElements.java
+++ levitskiiProj/src/main/java/libs/ActionsWithOurElements.java
@@ -21,18 +21,22 @@ public class ActionsWithOurElements {
             webElement.sendKeys(text);
             logger.info(text + " was inrupted into element");
         }catch (Exception e) {
-        logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
+            printErrorAndStopTest();
         }
 
     }
+
+    private void printErrorAndStopTest() {
+        logger.error("Can not work with element");
+        Assert.fail("Can not work with element");
+    }
+
     public void clickOnElement (WebElement webElement){
         try {
             webElement.click();
             logger.info("Button was clicked");
         }catch (Exception e) {
-            logger.error("Can not work with element");
-            Assert.fail("Can not work with element");
+            printErrorAndStopTest();
 
         }
     }
--- levitskiiProj/src/main/java/pages/LogInPage.java
+++ levitskiiProj/src/main/java/pages/LogInPage.java
@@ -4,8 +4,17 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class LogInPage extends ParentPage{
+    @FindBy(name ="_username")
+    private WebElement inputLogin;
+
+    @FindBy(id = "password")
+    private WebElement inputPassword;
+
+    @FindBy (tagName = "button")
+    private WebElement buttonSubmit;
 
 
     public LogInPage(WebDriver webDriver) {
@@ -22,15 +31,15 @@ public class LogInPage extends ParentPage{
         }
     }
         public void enterLogin (String login){
-            WebElement inputLogin = webDriver.findElement(By.name("_username"));
+  //          WebElement inputLogin = webDriver.findElement(By.name("_username"));
             actionsWithOurElements.enterTextIntoElement(inputLogin, login);
         }
         public void enterPass(String password){
-            WebElement inputPassword = webDriver.findElement(By.id("password"));
+  //      WebElement inputPassword = webDriver.findElement(By.id("password"));
             actionsWithOurElements.enterTextIntoElement(inputPassword, password);
         }
         public void clickSubmitButton(){
-        WebElement buttonSubmit = webDriver.findElement(By.tagName("button"));
+   //     WebElement buttonSubmit = webDriver.findElement(By.tagName("button"));
         actionsWithOurElements.clickOnElement(buttonSubmit);
         }
 }
