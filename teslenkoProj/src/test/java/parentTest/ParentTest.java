fb2a83702ca5569fc3d22d53d2b6081a73fe913f
Olena.Teslenko <alenateslenko13@gmail.com>
2018-04-17 21:31:31 +0300

    commit 17-04

--- teslenkoProj/src/test/java/parentTest/ParentTest.java
+++ teslenkoProj/src/test/java/parentTest/ParentTest.java
@@ -1,9 +1,12 @@
 package parentTest;
 
+import org.apache.log4j.Logger;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 import pages.LoginPage;
 
 import java.io.File;
@@ -12,6 +15,8 @@ import java.util.concurrent.TimeUnit;
 public class ParentTest {
     WebDriver webdriver;
     protected LoginPage loginPage;
+    protected HomePage homePage;
+    Logger logger = Logger.getLogger(getClass());
 
     @Before
     public void setUp() {
@@ -23,12 +28,20 @@ public class ParentTest {
         webdriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
         loginPage = new LoginPage(webdriver);
+        homePage = new HomePage(webdriver);
 
     }
 
     @After
     public void tearDown() {
         webdriver.quit();
+    }
+
+    protected void  checkAC(String message, boolean actual, boolean expected){
+        if(!(actual == expected)) {
+            logger.error("AC failed:" + message);
+        }
+        Assert.assertEquals(message, expected, actual);
     }
 
 }
