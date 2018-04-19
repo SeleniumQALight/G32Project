d3dc6368249376e7ec43eefa51f02c01e5d2d54e
Levitskiy <mlevitskiy89@gmail.com>
2018-04-19 22:03:04 +0300

    19/04

--- levitskiiProj/src/main/java/pages/HomePage.java
+++ levitskiiProj/src/main/java/pages/HomePage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -8,13 +9,26 @@ import org.openqa.selenium.support.FindBy;
 public class HomePage extends ParentPage {
     @FindBy(xpath=".//div[@class='pull-left image']//img[@class='img-circle']")
     private WebElement avatar;
-
+    @FindBy (xpath=".//*[@id='dictionary']/a")
+    private WebElement menuDictionary;
+    @FindBy (xpath=".//*[@id='spares'']")
+    private WebElement subMenuSpares;
     public HomePage(WebDriver webDriver) {
         super(webDriver);
     }
-
+    public void clickElementDictionary() {
+        actionsWithOurElements.clickOnElement(menuDictionary);
+    }
+    public void clickOnSubMenuSpares(){
+        actionsWithOurElements.clickOnElement(subMenuSpares);
+    }
     public boolean isAvatarPresent(){
       return actionsWithOurElements.isElementPresent(avatar);
 
     }
-}
+
+    public void checkAvatarIsPresent() {
+        Assert.assertTrue("Avatar is not present", isAvatarPresent());
+    }
+
+    }
