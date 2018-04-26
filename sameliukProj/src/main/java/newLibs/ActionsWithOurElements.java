be7e6a1e6f1b74be633480abafc06fe7c0326661
sameliuk <samov4448@gmail.com>
2018-04-26 21:47:08 +0300

    Added commit 26.04

--- sameliukProj/src/main/java/newLibs/ActionsWithOurElements.java
+++ sameliukProj/src/main/java/newLibs/ActionsWithOurElements.java
@@ -5,24 +5,28 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.awt.*;
+import java.util.List;
 
 public class ActionsWithOurElements {
     WebDriver webDriver;
     Logger logger;
+    WebDriverWait webDriverWait15;
 
 
     public ActionsWithOurElements(WebDriver webDriver) {
         this.webDriver = webDriver;
         logger = Logger.getLogger(getClass());
-        WebElement checkbox;
+        webDriverWait15 = new WebDriverWait(webDriver, 15);
+
     }
 
     public void enterTextIntoElement(WebElement webElement, String text) {
         try {
+            webDriverWait15.until(ExpectedConditions.visibilityOf(webElement));
             webElement.clear();
             webElement.sendKeys(text);
             logger.info(text + " was inputted into element");
@@ -34,9 +38,10 @@ public class ActionsWithOurElements {
 
     public void clickOnElement(WebElement webElement) {
         try {
+            webDriverWait15.until(ExpectedConditions.elementToBeClickable(webElement));
             webElement.click();
             logger.info("element was clicked");
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
     }
@@ -48,16 +53,17 @@ public class ActionsWithOurElements {
     }
 
     public boolean isElementPresent(WebElement webElement) {
-        try{
+        try {
             return webElement.isDisplayed() && webElement.isEnabled();
-        }catch (Exception e){
+        } catch (Exception e) {
             return false;
         }
 
     }
 
     /**
-     *  Method select value in DD
+     * Method select value in DD
+     *
      * @param webElement
      * @param value
      */
@@ -67,26 +73,69 @@ public class ActionsWithOurElements {
             Select select = new Select(webElement);
             select.selectByValue(value);              // or select by visible text if needed
             logger.info(value + "was select in DD");
-        }catch (Exception e){
+        } catch (Exception e) {
             printErrorAndStopTest(e);
         }
 
+    }
+
+
+    /**
+     * NeededSatete can be only "check" or "uncheck"
+     */
+    public void setCheckBoxToNeededState(WebElement webElement, String neededState) {
+
+        try{
+            boolean isCheckState = "check".equals(neededState);
+            boolean isUnCheckState = "uncheck".equals(neededState);
+            if (isCheckState || isUnCheckState){
+                if (webElement.isSelected() && isCheckState){
+                    logger.info("Check box is checked already");
+                } else if (webElement.isSelected() && isUnCheckState){
+                    clickOnElement(webElement);
+                } else if (!webElement.isSelected() && isCheckState){
+                    clickOnElement(webElement);
+                } else if (!webElement.isSelected() && isUnCheckState){
+                    logger.info("Check box is unchecked already");
+                }
+
+            } else {
+                logger.error(neededState + " should be 'check' or 'uncheck'");
+                Assert.fail(neededState + " should be 'check' or 'uncheck'");
             }
 
-
-    /** NeededSatete can be only "check" or "uncheck"
-     */
-    public void setCheckBoxToNeededState(WebElement webElement, String neededState){
-        Checkbox checkbox = new Checkbox(webElement);
-        try{
-            webDriver.get("http://v3.test.itpmgroup.com/dictionary/providers/edit/1");
-           ( webDriver.findElement(By.xpath(".//input[@id='prov_cus_proCustIsFl']")).click());
-
-          Assert.assertTrue("checkbox is check", checkbox.isEnabled();
-
-        }catch (Exception e){
+        }catch(Exception e){
             printErrorAndStopTest(e);
+        }
+    }
 
+    public boolean isElementPresent(String locator) {
+        try {
+            WebElement webElement = webDriver.findElement(By.xpath(locator));
+            return isElementPresent(webElement);
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public boolean isElementInList(String locator) {
+        try{
+            List<WebElement> listOfElements=webDriver.findElements(By.xpath(locator));
+            if (listOfElements.size()>0){
+                return true;
+            }else {
+                return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public void clickOnElement(String locator) {
+        try {
+            clickOnElement(webDriver.findElement(By.xpath(locator)));
+        }catch (Exception e ){
+            printErrorAndStopTest(e);
         }
     }
 }
