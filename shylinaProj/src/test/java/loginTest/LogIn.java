ebbbf12d01b185a8b0b510268891891056549064
Tanya Shylina <t.shylina@gmail.com>
2018-05-12 22:17:33 +0300

    не подключается хромдрайвер

--- shylinaProj/src/test/java/loginTest/LogIn.java
+++ shylinaProj/src/test/java/loginTest/LogIn.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 import java.io.File;
---
+++ shylinaProj/src/test/java/spareTest/addNewSpareTest.java
@@ -0,0 +1,36 @@
+package spareTest;
+
+import org.junit.After;
+import org.junit.Test;
+import parentTest.ParentTest;
+
+public class addNewSpareTest extends ParentTest {
+    final String nameOfNewSpare = "testSpare8";
+
+    @Test
+    public void addNewSpare() {
+        loginPage.userLogin("student", "909090");
+        homePage.checkAvatarIsPresent();
+        homePage.checkCurrentUrl();
+        homePage.clickOnMenuDictionary();
+        homePage.clickOnSubMenuSpare();
+        sparesPage.checkCurrentUrl();
+        sparesPage.deletingAllSparesWithName(nameOfNewSpare);
+        sparesPage.clicOnButtoPlus();
+        editSparesPage.checkCurrentUrl();
+        editSparesPage.enterSpareName(nameOfNewSpare);
+        editSparesPage.selectSpareType("4");
+        editSparesPage.clickButtoreate();
+        editSparesPage.checkCurrentUrl();
+
+        checkAC("New spare wasn't added", sparesPage.isNewSpareAdded(nameOfNewSpare), true);
+    }
+
+    @After
+    public void deletingNewSpare() {
+        sparesPage.deletingAllSparesWithName(nameOfNewSpare);
+    }
+
+}
+
+
